fe43933b51b262ad52227eb4642597c0043f6d65
Angela Guardia <angelaguardia20@gmail.com>
2020-07-30 16:34:58 -0700

    Add output message to NightWriter

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,6 @@
 class NightWriter
-  
+  original_file = ARGV[0]
+  original_data = File.read(original_file)
+  translated_file = ARGV[1]
+  puts "Created '#{translated_file}' containing #{original_data.length} characters"
 end
