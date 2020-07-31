1e4db3d0aee6db39355ae805e0c5de942d15af09
Angela Guardia <angelaguardia20@gmail.com>
2020-07-30 18:51:58 -0700

    Add test for read method to NightWriterTest

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,12 +1,22 @@
 require './test/test_helper'
 require './lib/night_writer'
+require 'mocha/minitest'
 
 class NightWriterTest < Minitest::Test
   def setup
+    NightWriter.any_instance.stubs(:print_output)
     @writer = NightWriter.new
   end
 
   def test_it_exists
     assert_instance_of NightWriter, @writer
+  end
+
+  def test_it_takes_in_command_line_arguments
+    assert_equal 2, ARGV.length
+  end
+
+  def test_assert_it_can_read_a_file
+    assert_instance_of String, @writer.plain_text
   end
 end
