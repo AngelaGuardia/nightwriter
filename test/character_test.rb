6a5ad3ff7a479d5bf6750233b09a7ea7649fe22f
Angela Guardia <angelaguardia20@gmail.com>
2020-07-31 10:32:40 -0700

    Add test for attributes to CharacterTest

--- test/character_test.rb
+++ test/character_test.rb
@@ -3,10 +3,15 @@ require './lib/character'
 
 class CharacterTest < Minitest::Test
   def setup
-    @character = Character.new
+    @character = Character.new("b", "12")
   end
 
   def test_it_exists
     assert_instance_of Character, @character
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal "b", @character.symbol
+    assert_equal ["1", "2"], @character.dots
   end
 end
