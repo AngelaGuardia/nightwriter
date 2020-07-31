1514a4656e1fdac151aee2b25bb61875bf7ec925
Angela Guardia <angelaguardia20@gmail.com>
2020-07-31 09:02:13 -0700

    Add readable attributes to NightWriter

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,18 +1,24 @@
 class NightWriter
-  attr_reader :plain_text
+  attr_reader :plain_filename,
+              :braille_filename,
+              :plain_text
 
   def initialize
+    @plain_filename = ARGV[0]
+    @braille_filename = ARGV[1]
     read
   end
 
   def read
-    plain_filename = ARGV[0]
-    @plain_text = File.read(plain_filename).delete("\n")
-    braille_filename = ARGV[1]
-    print_output(braille_filename, plain_text)
+    @plain_text = File.read(@plain_filename).delete("\n")
+    print_output(@braille_filename, plain_text)
   end
 
   def print_output(braille_filename, plain_text)
     puts "Created '#{braille_filename}' containing #{plain_text.length} characters"
+  end
+
+  def write
+
   end
 end
