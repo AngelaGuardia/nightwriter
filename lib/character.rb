56f2fccebf4a4f2748327bd023c7d4057a6e6939
Angela Guardia <angelaguardia20@gmail.com>
2020-07-31 10:47:48 -0700

    Add create_dots method to Character

--- lib/character.rb
+++ lib/character.rb
@@ -1,9 +1,20 @@
+require './lib/dot'
+
 class Character
   attr_reader :symbol,
+              :dot_nums,
               :dots
 
-  def initialize(symbol, dots)
+  def initialize(symbol, dot_nums)
     @symbol = symbol
-    @dots = dots.split(//)
+    @dot_nums = dot_nums.split(//)
+    @dots = []
+    create_dots
+  end
+
+  def create_dots
+    6.times do |i|
+      @dots << Dot.new(i.to_s)
+    end
   end
 end
