e4f3b9f5f2c9cd6d7fc1fb61f26384f15147593e
Angela Guardia <angelaguardia20@gmail.com>
2020-07-31 11:01:25 -0700

    Add turn_on_dots method to Character

--- lib/character.rb
+++ lib/character.rb
@@ -8,13 +8,18 @@ class Character
   def initialize(symbol, dot_nums)
     @symbol = symbol
     @dot_nums = dot_nums.split(//)
-    @dots = []
+    @dots = {}
     create_dots
+    turn_on_dots
   end
 
   def create_dots
     6.times do |i|
-      @dots << Dot.new(i.to_s)
+      @dots[(i+1).to_s] = Dot.new(i.to_s)
     end
+  end
+
+  def turn_on_dots
+    dot_nums.each { |num| @dots[num].turn_on }
   end
 end
