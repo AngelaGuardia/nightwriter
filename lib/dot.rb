30ea0aabc27cc8458c36720b7be72ea3fffc8626
Angela Guardia <angelaguardia20@gmail.com>
2020-07-31 09:57:59 -0700

    Add turn_on method to Dot

--- lib/dot.rb
+++ lib/dot.rb
@@ -1,9 +1,13 @@
 class Dot
   attr_reader :number,
               :is_on
-              
+
   def initialize(number)
     @number = number
     @is_on = false
+  end
+
+  def turn_on
+    @is_on = true
   end
 end
