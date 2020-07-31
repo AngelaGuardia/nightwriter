623cdf967ab01455cd47a52c60d4801b9ecb8212
Angela Guardia <angelaguardia20@gmail.com>
2020-07-31 08:43:34 -0700

    Add test for readable attributes to NightWriterTest

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,5 +1,6 @@
 require './test/test_helper'
 require './lib/night_writer'
+require 'minitest/stub_const'
 require 'mocha/minitest'
 
 class NightWriterTest < Minitest::Test
@@ -14,6 +15,14 @@ class NightWriterTest < Minitest::Test
 
   def test_it_takes_in_command_line_arguments
     assert_equal 2, ARGV.length
+  end
+
+  def test_it_has_readable_attributes
+    NightWriter.stub_const(:ARGV, ["plain_file", "braille_file"]) do
+      NightWriter::ARGV
+    end
+    assert_equal "plain_file", @writer.plain_filename
+    assert_equal "braille_file", @writer.braille_filename
   end
 
   def test_assert_it_can_read_a_file_and_has_readable_attr
