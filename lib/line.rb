b07be782b9fc438580395f5be783b0ffeba40434
Angela Guardia <angelaguardia20@gmail.com>
2020-07-31 15:07:01 -0700

    Add generate_characters method to Line

--- lib/line.rb
+++ lib/line.rb
@@ -1,8 +1,12 @@
+require './lib/character'
+require './lib/dictionary'
+
 class Line
   attr_reader :characters
 
   def initialize
     @characters = []
+    @dictionary = Dictionary.new
   end
 
   def add_character(character)
@@ -11,5 +15,11 @@ class Line
 
   def length
     @characters.size
+  end
+
+  def generate_characters(symbols)
+    symbols.each do |symbol|
+      add_character(Character.new(symbol, @dictionary.get(symbol)))
+    end
   end
 end
