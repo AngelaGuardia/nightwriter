b5de9b2e473d1a92212f44bb306180faab619ca4
Angela Guardia <angelaguardia20@gmail.com>
2020-07-31 11:47:53 -0700

    Add generate method to Dictionary

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,2 +1,16 @@
+require 'csv'
+
 class Dictionary
+  attr_reader :dictionary
+
+  def initialize
+    @dictionary = {}
+    generate
+  end
+
+  def generate
+    CSV.foreach('./data/braille_dots.csv', headers: true, header_converters: :symbol) do |row|
+      @dictionary[row[:character]] = row[:nums]
+    end
+  end
 end
