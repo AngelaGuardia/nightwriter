40f592e1a1afe80f79a75b122ee188439278f3ce
Angela Guardia <angelaguardia20@gmail.com>
2020-07-30 18:52:20 -0700

    Add read and print_out method to NightWriter

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,6 +1,18 @@
 class NightWriter
-  original_file = ARGV[0]
-  original_data = File.read(original_file)
-  translated_file = ARGV[1]
-  puts "Created '#{translated_file}' containing #{original_data.length} characters"
+  attr_reader :plain_text
+
+  def initialize
+    read
+  end
+
+  def read
+    plain_filename = ARGV[0]
+    @plain_text = File.read(plain_filename).delete("\n")
+    braille_filename = ARGV[1]
+    print_output(braille_filename, plain_text)
+  end
+
+  def print_output(braille_filename, plain_text)
+    puts "Created '#{braille_filename}' containing #{plain_text.length} characters"
+  end
 end
