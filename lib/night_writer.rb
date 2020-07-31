c492def27c19dd7b9a249b03b3d238ab7abf4c6c
Angela Guardia <angelaguardia20@gmail.com>
2020-07-31 09:20:32 -0700

    Add write method and update attribute names

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,24 +1,27 @@
 class NightWriter
   attr_reader :plain_filename,
               :braille_filename,
-              :plain_text
+              :plain_file,
+              :braille_file
 
   def initialize
     @plain_filename = ARGV[0]
     @braille_filename = ARGV[1]
     read
+    write
   end
 
   def read
-    @plain_text = File.read(@plain_filename).delete("\n")
-    print_output(@braille_filename, plain_text)
+    @plain_file = File.read(@plain_filename).delete("\n")
+    print_output(@braille_filename, plain_file)
   end
 
-  def print_output(braille_filename, plain_text)
-    puts "Created '#{braille_filename}' containing #{plain_text.length} characters"
+  def print_output(braille_filename, plain_file)
+    puts "Created '#{braille_filename}' containing #{plain_file.length} characters"
   end
 
   def write
-
+    @braille_text = "test"
+    @braille_file = File.open(@braille_filename, "w") { |f| f.write(@braille_text) }
   end
 end
