21672257bd8563ec66592760901f88f72f9d00cf
Angela Guardia <angelaguardia20@gmail.com>
2020-08-01 13:58:13 -0700

    Delete cmd line arg test and refactor attr test

--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -10,14 +10,14 @@ class NightReaderTest < Minitest::Test
     assert_instance_of NightReader, reader
   end
 
-  def test_it_takes_in_command_line_arguments
-    assert_equal true, ARGV.length > 0
-  end
-
   def test_it_can_read_filename_attributes
-    reader = NightReader.new
+    Object.stub_const(:ARGV, ["braille", "translated"]) do
+      reader = NightReader.new
 
-    assert_equal "braille.txt", reader.braille_filename
-    assert_equal "original_message.txt", reader.translated_filename
+      assert_equal "braille", reader.braille_filename
+      assert_equal "translated", reader.translated_filename
+    end
   end
+
+
 end
