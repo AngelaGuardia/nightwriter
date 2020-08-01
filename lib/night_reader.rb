b35e69a20e0b58ede936e69ba9e1b3ff3bfce074
Angela Guardia <angelaguardia20@gmail.com>
2020-08-01 14:14:21 -0700

    Add read method to NightReader

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,9 +1,13 @@
 class NightReader
   attr_reader :braille_filename,
               :translated_filename
-              
+
   def initialize
     @braille_filename = ARGV[0]
     @translated_filename = ARGV[1]
+  end
+
+  def read
+    @braille_file = File.read(@braille_filename).chomp
   end
 end
