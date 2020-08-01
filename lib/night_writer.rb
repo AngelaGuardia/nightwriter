c23475117972d0a465cb5c438823601ca7833ecd
Angela Guardia <angelaguardia20@gmail.com>
2020-07-31 18:01:49 -0700

    Add write_braille method to NightWriter

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,5 @@
+require './lib/line'
+
 class NightWriter
   attr_reader :plain_filename,
               :braille_filename,
@@ -17,8 +19,20 @@ class NightWriter
   end
 
   def write
-    @braille_text = "test"
+    @braille_text = write_braille
     @braille_file = File.open(@braille_filename, "w") { |f| f.write(@braille_text) }
+  end
+
+  def write_braille
+    text = ""
+    strings = @plain_file.downcase.scan(/.{1,40}/)
+    strings.each do |string|
+      line = Line.new(string)
+      line.generate_characters
+      text += line.render
+      text += "\n" unless string == strings.last
+    end
+    text
   end
 
   def print_output(braille_filename, plain_file)
