2e4a5a09738c9ab938207d1e79fac4becb7f1854
Angela Guardia <angelaguardia20@gmail.com>
2020-08-01 13:53:59 -0700

    Add test for attributes to NightReaderTest

--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -6,10 +6,18 @@ require 'mocha/minitest'
 class NightReaderTest < Minitest::Test
   def test_it_exists
     reader = NightReader.new
+
     assert_instance_of NightReader, reader
   end
 
   def test_it_takes_in_command_line_arguments
     assert_equal true, ARGV.length > 0
+  end
+
+  def test_it_can_read_filename_attributes
+    reader = NightReader.new
+
+    assert_equal "braille.txt", reader.braille_filename
+    assert_equal "original_message.txt", reader.translated_filename
   end
 end
