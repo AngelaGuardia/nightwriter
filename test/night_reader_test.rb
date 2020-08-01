e0bc6391acf3fd7528edd3211fff3e4c664a25a7
Angela Guardia <angelaguardia20@gmail.com>
2020-08-01 13:49:59 -0700

    Add test for command line arguments to NightReaderTest

--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -8,4 +8,8 @@ class NightReaderTest < Minitest::Test
     reader = NightReader.new
     assert_instance_of NightReader, reader
   end
+
+  def test_it_takes_in_command_line_arguments
+    assert_equal true, ARGV.length > 0
+  end
 end
