7fbb12ce565a2f59c8c2cc0f82ba09752c75c2cb
Angela Guardia <angelaguardia20@gmail.com>
2020-08-01 14:23:30 -0700

    Add write method to NightReader

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,6 +1,7 @@
 class NightReader
   attr_reader :braille_filename,
-              :translated_filename
+              :translated_filename,
+              :translated_file
 
   def initialize
     @braille_filename = ARGV[0]
@@ -9,5 +10,11 @@ class NightReader
 
   def read
     @braille_file = File.read(@braille_filename).chomp
+  end
+
+  def write
+    @translated_text = translate_braille
+    File.open(@translated_filename, "w") { |f| f.write(@translated_text) }
+    @translated_file = File.read(@translated_filename)
   end
 end
