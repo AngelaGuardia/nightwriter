c12d507c3b747be545be705eea3333abe590c85e
Angela Guardia <angelaguardia20@gmail.com>
2020-08-01 13:54:11 -0700

    Add attributes to NightReader

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,2 +1,9 @@
 class NightReader
+  attr_reader :braille_filename,
+              :translated_filename
+              
+  def initialize
+    @braille_filename = ARGV[0]
+    @translated_filename = ARGV[1]
+  end
 end
