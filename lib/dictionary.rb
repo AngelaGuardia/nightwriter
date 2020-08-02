dbe96b7d8bed65f343d538b1adce11c4377bb045
Angela Guardia <angelaguardia20@gmail.com>
2020-08-01 17:20:26 -0700

    Add generate_translator method to Dictionary

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -3,14 +3,25 @@ require 'csv'
 class Dictionary
   attr_reader :dictionary
 
-  def initialize
+  def initialize(translator_mode = false)
     @dictionary = {}
-    generate
+    @mode = translator_mode
+    if translator_mode
+      generate_translator
+    else
+      generate
+    end
   end
 
   def generate
     CSV.foreach('./data/braille_dots.csv', headers: true, header_converters: :symbol) do |row|
       @dictionary[row[:character]] = row[:nums]
+    end
+  end
+
+  def generate_translator
+    CSV.foreach('./data/braille_dots.csv', headers: true, header_converters: :symbol) do |row|
+      @dictionary[row[:nums]] = row[:character]
     end
   end
 
